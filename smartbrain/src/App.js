e04e36611e7c2e7eee92fa6a2c7cb411b1cfbd54
Saad Hussain Syed <90135919+Saad3123@users.noreply.github.com>
2024-10-16 23:34:13 +0000

    adding stuff from vid 1

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+import Rank from './components/Rank/Rank';
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim"; // Assuming you want to use the slim version of tsparticles
 
-function App() {
+const App = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadSlim(engine); // Load only slim version of tsparticles
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    console.log(container);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles
+  id="tsparticles"
+  init={particlesInit}
+  loaded={particlesLoaded}
+  options={{
+    particles: {
+      number: {
+        value: 50,
+      },
+      size: {
+        value: 3,
+      },
+      move: {
+        speed: 1,
+      },
+      line_linked: {
+        enable: true,
+      },
+    },
+  }}
+/>
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <Rank />
     </div>
   );
 }
